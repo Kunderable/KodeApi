766ec118c3de8bb53a35ff1439b529c9aa292929
Ilya Sutormin <dyadyajobs@gmail.com>
2022-10-07 12:35:42 +0500

    Initial Commit

---
+++ Kode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kode
+//
+//  Created by Илья Сутормин on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
